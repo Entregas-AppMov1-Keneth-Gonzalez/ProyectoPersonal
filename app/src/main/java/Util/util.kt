88b75d53027a43d8f35bed30a3e75735aafcb98c
root <kegonzalezca@est.utn.ac.cr>
2024-10-29 22:00:40 -0600

    arreglos

--- app/src/main/java/Util/util.kt
+++ app/src/main/java/Util/util.kt
@@ -1,2 +1,16 @@
 package Util
 
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+
+const val EXTRA_MESSAGE_CONTACT_ID = "cr.ac.utn.contactmanager.contactID"
+
+class util {
+    companion object{
+        fun openActivity(context: Context, objClass: Class<*>, keyName: String = "", value: String? = ""){
+            val intent = Intent(context, objClass).apply { putExtra(keyName, value)}
+            startActivity(context, intent, null)
+        }
+    }
+}
--- app/src/main/java/cr/ac/utn/kenethgonzalez/proyectopersonal/MainActivity.kt
+++ app/src/main/java/cr/ac/utn/kenethgonzalez/proyectopersonal/MainActivity.kt
@@ -1,7 +1,12 @@
 package cr.ac.utn.kenethgonzalez.proyectopersonal
 
+import Util.util
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,5 +21,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnLogin: Button = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener(View.OnClickListener {
+            util.openActivity(this, LoginActivity::class.java)
+        })
+
+        val btnMainDialog: Button = findViewById<Button>(R.id.btnCloseDialog)
+        btnMainDialog.setOnClickListener(View.OnClickListener {
+            DisplayCloseDialog()
+        })
+
+    }
+
+    private fun DisplayCloseDialog(){
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage(R.string.closedialog).setCancelable(false).setPositiveButton(R.string.yes, DialogInterface.OnClickListener{
+                dialog, id -> finish()
+        })
+            .setNegativeButton(R.string.no, DialogInterface.OnClickListener{
+                    dialog, id -> dialog.cancel()
+            })
+
+        val alert = dialogBuilder.create()
+        alert.setTitle(R.string.closedialog)
+        alert.show()
     }
 }
